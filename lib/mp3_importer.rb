953f1a8df0ce5e157901c4d9b5c4e64fc4e7599d
Ron Lynch <lynch.ronald1@gmail.com>
2019-02-04 02:06:18 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,34 +1,34 @@
 class MP3Importer
 
-  # attr_reader :path
-  #
-  # def initialize(filepath)
-  #   @path = filepath
-  # end
-  #
-  # def files
-  #   @files = Dir.entries(@path)
-  #   @files.delete_if {|file| file == "." || file == ".."}
-  # end
-  #
-  # def import
-  #   self.files.each do |file|
-  #     song = Song.new_by_filename(file)
-  #     Artist.all << song.artist unless Artist.all.include?(song.artist)
-  #   end
-  # end
-  attr_accessor :path, :files
+  attr_reader :path
 
-   def initialize(path)
-    @path = path
+  def initialize(filepath)
+    @path = filepath
   end
 
-   def files
-    Dir.entries(@path).select!{ |file| file.end_with?("mp3") }
+  def files
+    @files = Dir.entries(@path)
+    @files.delete_if {|file| file == "." || file == ".."}
   end
 
-   def import
-    files.each { |file| Song.new_by_filename(file) }
+  def import
+    self.files.each do |file|
+      song = Song.new_by_filename(file)
+      Artist.all << song.artist unless Artist.all.include?(song.artist)
+    end
   end
+  # attr_accessor :path, :files
+  #
+  #  def initialize(path)
+  #   @path = path
+  # end
+  #
+  #  def files
+  #   Dir.entries(@path).select!{ |file| file.end_with?("mp3") }
+  # end
+  #
+  #  def import
+  #   files.each { |file| Song.new_by_filename(file) }
+  # end
 
  end
